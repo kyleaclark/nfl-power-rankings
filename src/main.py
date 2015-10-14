ba961ba01c8fb24445d782e52699527ef67f6c10
Kyle Clark <kyle.andrew.clark@gmail.com>
2015-10-13 19:54:50 -0700

    Added strength of victory and other adjustments

--- src/main.py
+++ src/main.py
@@ -13,13 +13,10 @@ def generate_query(team):
     'Generate a base query to the NFLDB'
 
     team_query = nfldb.Query(db)
-    team_query.game(season_year=2015, season_type='Regular', team=team['id'], week=[1, 2, 3, 4])
+    team_query.game(season_year=2015, season_type='Regular', team=team['id'], week=[1, 2, 3, 4, 5])
 
     for game in team_query.as_games():
         team['games'] += 1
-
-        if game.winner == team['id']:
-            team['wins'] += 1
 
         if game.home_team == team['id']:
             team['points_scored'] += game.home_score
@@ -27,6 +24,13 @@ def generate_query(team):
             team['point_differential'] += (game.home_score - game.away_score)
             team['turnover_differential'] += (game.home_turnovers - game.away_turnovers)
             team['opponents'].append(game.away_team)
+
+            if game.winner == game.home_team:
+                team['wins'] += 1
+                team['game_win_opponents'].append(game.away_team)
+            else:
+                team['losses'] += 1
+
         else:
             team['points_scored'] += game.away_score
             team['points_against'] += game.home_score
@@ -34,15 +38,35 @@ def generate_query(team):
             team['turnover_differential'] += (game.away_turnovers - game.home_turnovers)
             team['opponents'].append(game.home_team)
 
+            if game.winner == game.away_team:
+                team['wins'] += 1
+                team['game_win_opponents'].append(game.home_team)
+            else:
+                team['losses']
+
 def calc_team_record_ranking(team):
     for opp in team['opponents']:
         team['opponent_games'] += teams[opp]['games']
         team['opponent_wins'] += teams[opp]['wins']
 
+    for opp in team['game_win_opponents']:
+        team['game_win_opponents_games'] += teams[opp]['games']
+        team['game_win_opponents_wins'] += teams[opp]['wins']
+
     team['points_scored_avg'] = team['points_scored'] / team['games']
     team['points_against_avg'] = team['points_against'] / team['games']
     team['point_differential_avg'] = team['point_differential'] / team['games']
-    team['sos'] = team['opponent_wins'] / team['opponent_games']
+
+    if team['opponent_wins'] > 0:
+        team['sos'] = team['opponent_wins'] / team['opponent_games']
+    else:
+        team['sos'] = 0
+
+    if team['game_win_opponents_wins']:
+        team['sov'] = team['game_win_opponents_wins'] / team['game_win_opponents_games']
+    else:
+        team['sov'] = 0
+
     team['win_percentage'] = team['wins'] / team['games']
 
 def calc_points_ranking(teams, pointsKey, rankingKey):
@@ -61,10 +85,10 @@ def calc_points_ranking(teams, pointsKey, rankingKey):
         index -= 1
 
 def calc_power_ranking(team):
-    team['win_value'] = ((team['wins'] * 2.5) * team['sos']) * 0.50
+    team['win_value'] = ((team['wins'] * 2.5) * team['sov']) * 0.50
     team['point_differential_value'] = (team['point_differential_avg'] * team['sos']) * 0.25
-    team['points_scored_value'] = ((team['points_scored_ranking'] * 0.3125) * team['sos']) * 0.10
-    team['points_against_value'] = ((team['points_against_ranking'] * 0.3125) * team['sos']) * 0.10
+    team['points_scored_value'] = (team['points_scored_ranking'] * 0.3125) * 0.10
+    team['points_against_value'] = (team['points_against_ranking'] * 0.3125) * 0.10
     team['turnover_differential_value'] = team['turnover_differential'] * 0.05
 
     team['power_ranking'] = team['win_value'] + team['point_differential_value'] + team['points_scored_value'] + team['points_against_value'] + team['turnover_differential_value']
@@ -74,7 +98,7 @@ def print_power_rankings(teams):
 
     for id, team in sorted(teams.iteritems(), key=lambda (x, y): y['power_ranking'], reverse=True):
         # print '%d. %s, %.1f' % (index, team['id'], team['power_ranking'])
-        print '%d. %s, %.1f, wv: %.1f, pdv: %.1f, psv: %.1f, pav: %.1f, tdv: %.1f' % (index, team['id'], team['power_ranking'], team['win_value'], team['point_differential_value'], team['points_scored_value'], team['points_against_value'],  team['turnover_differential_value'])
+        print '%d. %s, %d-%d, %.2f, sos: %.2f, sov: %.2f, wv: %.2f, pdv: %.2f, psv: %.2f, pav: %.2f, tdv: %.2f' % (index, team['id'], team['wins'], team['losses'], team['power_ranking'], team['sos'], team['sov'], team['win_value'], team['point_differential_value'], team['points_scored_value'], team['points_against_value'],  team['turnover_differential_value'])
         index += 1
 
 init()
@@ -86,6 +110,7 @@ teams = team_data.get_data()
 for id, team in teams.iteritems():
     team['games'] = 0
     team['wins'] = 0
+    team['losses'] = 0
     team['points_scored'] = 0
     team['points_against'] = 0
     team['point_differential'] = 0
@@ -93,6 +118,9 @@ for id, team in teams.iteritems():
     team['opponents'] = []
     team['opponent_games'] = 0
     team['opponent_wins'] = 0
+    team['game_win_opponents'] = []
+    team['game_win_opponents_games'] = 0
+    team['game_win_opponents_wins'] = 0
     generate_query(team)
 
 for id, team in teams.iteritems():
